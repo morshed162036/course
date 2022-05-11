a2d60f26078522487248a749894ff8260b1a7e87
Morshed Ahmed <mahmed162036@bscse.uiu.ac.bd>
2022-05-11 18:40:36 +0600

    Create input.blade.php

---
+++ resources/views/components/input.blade.php
@@ -0,0 +1,21 @@
+{{-- <div class="form-group">
+        <label for="">Name</label>
+        <input type="text" name="name" id="" class="form-control" placeholder="" aria-describedby="helpId" value="{{ old('name') }}"/> 
+        <span class="text-danger">
+            @error('name')
+                {{ $message }}
+            @enderror
+        </span>
+        {{-- <small id="helpId" class="text-muted">Help Text</small--}
+</div> --}}
+<div class="form-group">
+        <label for="">{{ $label }}</label>
+        <input type="{{ $type }}" name="{{ $name }}" id="" class="form-control"/> 
+        <span class="text-danger">
+            {{ $demo }}
+            {{-- @error('{{ $name }}')
+                {{ $message }}
+            @enderror --}}
+        </span>
+        {{-- <small id="helpId" class="text-muted">Help Text</small> --}}
+</div>
